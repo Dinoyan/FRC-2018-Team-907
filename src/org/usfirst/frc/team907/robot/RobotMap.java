3d402f1b590fc838244b1bee2f10386b095ee87b
Dinoyan <dinoyan.g@hotmail.com>
2018-01-07 12:22:52 -0500

    updated map

--- src/org/usfirst/frc/team907/robot/RobotMap.java
+++ src/org/usfirst/frc/team907/robot/RobotMap.java
@@ -1,19 +1,19 @@
 package org.usfirst.frc.team907.robot;
 
 public class RobotMap {
-	
+
 	// Joysticks ports.
 	public static final int DRIVE_STICK = 0;
 	public static final int CUBE_STICK = 1;
-	
+
 	// Auto types.
 	public static final String DEFAULT_AUTO = "Default";
 	public static final String CUSTOM_AUTO = "My Auto";
-	
+
 	// Sensors.
 	public static final int RIGHT_ENC = 0;
 	public static final int LEFT_ENC = 1;
-	
+
 	// Drive motors.
 	public static final int RIGHT_DRIVE1 = 0;
 	public static final int RIGHT_DRIVE2 = 1;
@@ -21,7 +21,5 @@ public class RobotMap {
 	public static final int LEFT_DRIVE1 = 3;
 	public static final int LEFT_DRIVE2 = 4;
 	public static final int LEFT_DRIVE3 = 5;
-	
-	
 
 }
