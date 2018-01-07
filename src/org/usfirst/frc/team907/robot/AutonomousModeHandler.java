a2a2cb9a81ab6d90738504ddbfb362aaa0817678
Dinoyan <dinoyan.g@hotmail.com>
2018-01-07 12:22:32 -0500

    Started implementing Auto mode handler.

---
+++ src/org/usfirst/frc/team907/robot/AutonomousModeHandler.java
@@ -0,0 +1,23 @@
+package org.usfirst.frc.team907.robot;
+
+public class AutonomousModeHandler {
+	
+	public AutonomousModeHandler() {
+		
+	}
+	
+	public void  AutoState(String mode, String gameData) {
+	
+		switch (mode) {
+		case RobotMap.CUSTOM_AUTO:
+			// Put custom auto code here
+			break;
+		case RobotMap.DEFAULT_AUTO:
+		default:
+			// Put default auto code here
+			break;
+	}
+		
+	}
+
+}
