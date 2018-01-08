36588f21c5ba43a1b1f62f5c64c0a9316990fa45
Dinoyan <dinoyan.g@hotmail.com>
2018-01-08 12:21:10 -0500

    src format

--- src/org/usfirst/frc/team907/robot/LoggerData.java
+++ src/org/usfirst/frc/team907/robot/LoggerData.java
@@ -6,26 +6,25 @@ import java.util.logging.Logger;
 import java.util.logging.SimpleFormatter;
 
 public class LoggerData {
-	private final static Logger logger = Logger.getLogger(LoggerData.class
-		      .getName());
+	private final static Logger logger = Logger.getLogger(LoggerData.class.getName());
 	private static FileHandler fh = null;
-	
+
 	public static void logData(String data) {
-		
-		try {  
-	        fh = new FileHandler("/Users/dinoyan/Desktop/MyLogFile.log");  
-	        logger.addHandler(fh);
-	        SimpleFormatter formatter = new SimpleFormatter();  
-	        fh.setFormatter(formatter);  
 
-	    } catch (SecurityException e) {  
-	        e.printStackTrace();  
-	    } catch (IOException e) {  
-	        e.printStackTrace();  
-	    }  
+		try {
+			fh = new FileHandler("/Users/dinoyan/Desktop/MyLogFile.log");
+			logger.addHandler(fh);
+			SimpleFormatter formatter = new SimpleFormatter();
+			fh.setFormatter(formatter);
 
-	    logger.info(data);
-	    
+		} catch (SecurityException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		logger.info(data);
+
 	}
 
 }
