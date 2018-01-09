7c9abf747081d06af97f329083668d92c9340fb1
Dinoyan <dinoyan.g@hotmail.com>
2018-01-08 21:20:04 -0500

    Auto actions methods

--- src/org/usfirst/frc/team907/robot/AutonomousActions.java
+++ src/org/usfirst/frc/team907/robot/AutonomousActions.java
@@ -1,18 +1,25 @@
 package org.usfirst.frc.team907.robot;
 
+import com.kauailabs.navx.frc.AHRS;
+
+import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.Talon;
 
 public class AutonomousActions {
 
-	public static void driveForward(Talon test) {
+	public static void driveForward(Talon rDrive1, Talon rDrive2, Talon rDrive3, Talon lDrive1, Talon lDrive2,
+			Talon lDrive3, AHRS navx, Encoder rightEnc, Encoder leftEnc) {
+		System.out.println("Test: Drive Forward Working");
 
 	}
 
-	public static void turnRight() {
-
+	public static void turnRight(Talon rDrive1, Talon rDrive2, Talon rDrive3, Talon lDrive1, Talon lDrive2,
+			Talon lDrive3, AHRS navx, Encoder rightEnc, Encoder leftEnc) {
+		
 	}
 
-	public static void turnLeft() {
+	public static void turnLeft(Talon rDrive1, Talon rDrive2, Talon rDrive3, Talon lDrive1, Talon lDrive2,
+			Talon lDrive3, AHRS navx, Encoder rightEnc, Encoder leftEnc) {
 
 	}
 
