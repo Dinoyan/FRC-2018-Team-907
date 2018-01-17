f757f7cb971da5d49de95956c9e1998ad4d2804c
Dinoyan <dinoyan.g@hotmail.com>
2018-01-17 13:40:23 -0500

    Added RobotConstant class

---
+++ src/org/usfirst/frc/team907/robot/RobotConstant.java
@@ -0,0 +1,11 @@
+package org.usfirst.frc.team907.robot;
+
+public class RobotConstant {
+	
+	public static double DEFAULT_AUTO_DISTANCE = 0.0;
+	
+	public static double RIGHT_TURN_ANGLE = 0.0;
+	public static double LEFT_TURN_ANGLE = 0.0;
+	
+
+}
