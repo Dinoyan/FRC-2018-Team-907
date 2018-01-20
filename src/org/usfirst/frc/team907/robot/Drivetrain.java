3e4f655a4253a6ef8bc7ff0f41d1d8237b955e3d
Manish-GS <gs.manish002@gmail.com>
2018-01-20 10:53:48 -0500

    added the turn implentation

--- src/org/usfirst/frc/team907/robot/Drivetrain.java
+++ src/org/usfirst/frc/team907/robot/Drivetrain.java
@@ -1,32 +1,58 @@
 package org.usfirst.frc.team907.robot;
 
 public class Drivetrain {
-
+	
+	double left, right, t_left, t_right;
+	
 	public void driveRobot(JoystickHandler joystickHandler, MultiSpeedController multiSpeedController) {
-
-		double driverX = -joystickHandler.getDriveStick().getRawAxis(4);
-		double driverY = joystickHandler.getDriveStick().getRawAxis(1);
-
-		double leftOutput = driverY + driverX;
-		double rightOutput = driverX - driverY;
-
-		multiSpeedController.getlDrive1().set(leftOutput);
-		multiSpeedController.getlDrive2().set(leftOutput);
+				
+		double driveFront = joystickHandler.getDriveStick().getRawAxis(3);
+		double driveback = -joystickHandler.getDriveStick().getRawAxis(2);
+		double turn = joystickHandler.getDriveStick().getRawAxis(4);
 		
-		multiSpeedController.getrDrive1().set(rightOutput);
-		multiSpeedController.getrDrive2().set(rightOutput);
+		
+		if(driveFront > 0.0 && driveback == 0.0) {
+			t_left = driveFront + turn;
+			t_right = driveFront - turn;
+
+			left = t_left + skim(t_right);
+			right = t_right + skim(t_left);
+			
+			moveRobot(multiSpeedController, left, right);
+			
+		} else if(driveback > 0.0 && driveFront == 0.0) {
+			
+			t_left = driveFront + turn;
+			t_right = driveFront - turn;
+
+			left = t_left + skim(t_right);
+			right = t_right + skim(t_left);
+			
+			moveRobot(multiSpeedController, left, right);
+			
+		}else {
+			moveRobot(multiSpeedController, RobotConstant.STOP_ROBOT, RobotConstant.STOP_ROBOT);
+		}
 		
 		System.out.println("Hell Yeah!!!!!!");
 	}
 	
-	public void moveRobot(MultiSpeedController multiSpeedController, Double leftOutput, Double rightOutput) {
+	public void moveRobot(MultiSpeedController multiSpeedController, Double left, Double right) {
 		
-		multiSpeedController.getlDrive1().set(leftOutput);
-		multiSpeedController.getlDrive2().set(leftOutput);
+		multiSpeedController.getlDrive1().set(left);
+		multiSpeedController.getlDrive2().set(left);
 		
-		multiSpeedController.getrDrive1().set(rightOutput);
-		multiSpeedController.getrDrive2().set(rightOutput);
+		multiSpeedController.getrDrive1().set(right);
+		multiSpeedController.getrDrive2().set(right);
 		
 	}
+	
+	public double skim(double v) {
+	    if (v > 1.0) {
+	        return -((v - 1.0) * RobotConstant.TURNING_GAIN);
+	    } else if (v < -1.0) {
+	        return -((v + 1.0) * RobotConstant.TURNING_GAIN);
+	    } return 0; 
+	    }
 
 }
--- src/org/usfirst/frc/team907/robot/RobotConstant.java
+++ src/org/usfirst/frc/team907/robot/RobotConstant.java
@@ -7,4 +7,8 @@ public class RobotConstant {
 	public static double RIGHT_TURN_ANGLE = 0.0;
 	public static double LEFT_TURN_ANGLE = 0.0;
 	
+	public static double TURNING_GAIN = 1.0;
+	
+	public static double STOP_ROBOT = 0.0;
+	
 }
