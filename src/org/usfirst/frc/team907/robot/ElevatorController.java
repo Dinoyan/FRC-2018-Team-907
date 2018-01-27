2104985786b585611524493c18e3d0aa9c7238de
Dinoyan <dinoyan.g@hotmail.com>
2018-01-26 21:55:43 -0500

    Created elevator class with methodsignatures

---
+++ src/org/usfirst/frc/team907/robot/ElevatorController.java
@@ -0,0 +1,38 @@
+package org.usfirst.frc.team907.robot;
+
+import edu.wpi.first.wpilibj.Talon;
+
+public class ElevatorController {
+	private Talon elevCimOne;
+	private Talon elevCimTwo;
+	private SensorHandler sensorHandler;
+	private JoystickHandler joystickHandler;
+	
+	public ElevatorController(SensorHandler sensorHandler, JoystickHandler joystickHandler) {
+		elevCimOne = new Talon(RobotMap.ELEV_ONE);
+		elevCimTwo = new Talon(RobotMap.ELEV_TWO);
+		this.sensorHandler = sensorHandler;
+		this.joystickHandler = joystickHandler;
+	}
+	
+	public void startPositon() {
+		
+	}
+	
+	public void switchPosition() {
+		
+	}
+	
+	public void scalePosition() {
+		
+	}
+	
+	public void climbPosition() {
+		
+	}
+	
+	public void operateElevator() {
+		
+	}
+
+}
