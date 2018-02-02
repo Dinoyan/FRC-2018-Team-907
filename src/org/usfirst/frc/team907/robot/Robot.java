7ab0a193b0c99ac0fb5fb64ffdc6e1c4c2942153
Dinoyan <dinoyan.g@hotmail.com>
2018-02-02 00:32:09 -0500

    readToClimb -> finna vibrate joysticks =

--- src/org/usfirst/frc/team907/robot/Robot.java
+++ src/org/usfirst/frc/team907/robot/Robot.java
@@ -118,6 +118,8 @@ public class Robot extends IterativeRobot {
 		if (elevator.readyToClimb()) {
 			led.onGreen();
 			led.offRed();
+			joystickHandler.vibrateDriveStick();
+			joystickHandler.vibrateDriveStick();
 		} else {
 			led.onRed();
 			led.offRed();
